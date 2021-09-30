e24cdc48205fd09b936316948931eb78ae78b4e3
SanjivArora <sanjiv_arora@icloud.com>
2021-10-01 11:41:58 +1300

    Revert back the changes for DSC from VM Repo

--- PSModule/psake/psake-master/specs/using_required_when_not_set_should_fail.ps1
+++
@@ -1,9 +0,0 @@
-﻿properties {
-    $z = $null
-}
-
-task default -depends TestWithAction, TestWithoutAction
-
-task TestWithAction -requiredVariables z {}
-
-task TestWithoutAction -requiredVariables z
